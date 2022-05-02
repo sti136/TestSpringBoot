8c76705385b6d0d16d4e89005fe8287cf11d5ae7
sti136 <thomaslslosum@gmail.com>
2022-05-02 21:32:10 +0800

    REFORMAT CODE

--- src/main/java/com/sti136/helloworld/restful/dao/Greeting.java
+++ src/main/java/com/sti136/helloworld/restful/dao/Greeting.java
@@ -3,7 +3,8 @@ package com.sti136.helloworld.restful.dao;
 public class Greeting {
     private final long id;
     private final String content;
-    public Greeting(long id, String content){
+
+    public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
