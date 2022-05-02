5bffb89337ab74dc7fbf44ab9e18e452d924d4d9
sti136 <tlo@labci.com>
2022-05-02 20:06:04 +0800

    first commit

---
+++ src/test/java/com/sti136/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sti136.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
